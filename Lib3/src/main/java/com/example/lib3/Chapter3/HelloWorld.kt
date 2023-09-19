6f8b1be30a7b33d463d815b0fac74e4f37fd81c8
Lithium <rlawnsghdudw@naver.com>
2023-09-19 16:49:02 +0900

    Commit week2

---
+++ Lib3/src/main/java/com/example/lib3/Chapter3/HelloWorld.kt
@@ -0,0 +1,5 @@
+package com.example.lib3.Chapter3
+
+fun main() {
+    println("Hello World");
+}
--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.androidlab
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainText: TextView = findViewById(R.id.textView)
+        val btn1: Button = findViewById(R.id.button1)
+        val btn2: Button = findViewById(R.id.button2)
+
+        btn1.setOnClickListener {
+            mainText.text = "20195138"
+        }
+
+        btn2.setOnClickListener {
+            mainText.text = "김준호"
+        }
     }
 }
